113f87dae4c39f12b77845ae271070eeeba883e0
MAX FELDMAN <MAX FELDMAN@DESKTOP-56O7A44>
2018-04-08 01:26:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/maxfeldman/quiz_project/QuizActivity.java
@@ -0,0 +1,21 @@
+package com.example.maxfeldman.quiz_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+    }
+
+
+
+
+}
